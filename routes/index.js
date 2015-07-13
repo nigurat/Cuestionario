8f62c91c9730f4721f4443b508c96c5f3ab305a6
nigurat <nigurat18@gmail.com>
2015-07-13 16:23:16 -0300

    agrega autor

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Cuestionario' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Pantera Rosa' });
+});
+
 router.get('/cuestionarios/pregunta', cuestionarioControlador.pregunta);
 router.get('/cuestionarios/respuesta', cuestionarioControlador.respuesta);
 
